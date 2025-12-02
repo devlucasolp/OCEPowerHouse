6b1a31bd966dbb5492a54876570920d2e7459513
Lucas <devlucasolp@gmail.com>
2025-12-02 20:45:59 -0300

    feat(analytics): adicionar Microsoft Clarity no head e atualizar CSP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,12 +9,12 @@ export default function Document() {
         <link rel="icon" href="/img/static/favicon.png" type="image/png" />
 
         <script
+          id="clarity-script"
           dangerouslySetInnerHTML={{
             __html:
-              "(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window, document, 'clarity', 'script', 'ufg3vya8z2');",
+              '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "ufg3vya8z2");',
           }}
         />
-
       </Head>
       <body>
         <Main />
